44a2ee0d77aeb86bb981f6e1debaab166fdbc1c0
e143385 <e143385@66WP0S2>
2021-09-10 07:59:45 -0500

    Add project files.

---
+++ Login.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.grdData = new System.Windows.Forms.DataGridView();
+            this.btnImport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(17, 51);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(76, 13);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search Criteria";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(425, 67);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(99, 20);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Submit";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(20, 67);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(399, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // grdData
+            // 
+            this.grdData.AllowUserToOrderColumns = true;
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Location = new System.Drawing.Point(20, 96);
+            this.grdData.Name = "grdData";
+            this.grdData.Size = new System.Drawing.Size(504, 150);
+            this.grdData.TabIndex = 3;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(24, 17);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(500, 23);
+            this.btnImport.TabIndex = 4;
+            this.btnImport.Text = "Import Excel Data";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 260);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Name = "Login";
+            this.Text = "Login Form";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView grdData;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        private DataSet _dsData = null;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            this.SetControls();
+        }
+
+        public DataSet ExcelData 
+        {
+            set { _dsData = value; } 
+        }
+
+        private void SetControls()
+        {
+            //Labels
+            this.lblSearch.Text = "Search Criteria";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            ImportExcelData frmImport = new ImportExcelData();
+            frmImport.ShowDialog();
+        }
+    }
+}
